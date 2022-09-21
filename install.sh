ed05fb627794bcca57dfa04035af926c0b4bc2f4
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-21 10:03:21 +0600

    sd

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cp -r fonts/* ~/.local/share/fonts
 sudo apt install ibus-m17n
 sudo cp bn-bijoyClassic.png bn-bijoyUnicode.png /usr/share/m17n/icons
 sudo cp bn-bijoyUnicode.mim bn-bijoyClassic.mim /usr/share/m17n
-sudo gedit /var/lib/dpkg/info/m17n-db.list
+sudo gedit /var/lib/dpkg/info/m17n-db.list  ./copy2m17n-db.sh
